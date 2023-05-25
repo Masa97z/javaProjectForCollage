40baaca9c909e19dfb937d4ef933e9c8b1f5505e
masa <fmc97masa@gmail.com>
2023-05-26 01:57:41 +0300

    complete activity main and create fragmnt for add and list and home and profile and hundle xml to add and list fragment

--- app/src/main/java/com/masa/minisilm/MainActivity.java
+++ app/src/main/java/com/masa/minisilm/MainActivity.java
@@ -1,14 +1,23 @@
 package com.masa.minisilm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.masa.minisilm.Ui.AddFragment;
+import com.masa.minisilm.Ui.HomeFragment;
+import com.masa.minisilm.Ui.ListFragment;
+import com.masa.minisilm.Ui.ProfileFragment;
 import com.masa.minisilm.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +25,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
+        bottomNavigationView = binding.navigationView;
+        bottomNavigationView.setOnNavigationItemSelectedListener(navItemSelectedListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,
+                new HomeFragment()).commit();
+
     }
+    @SuppressLint("NonConstantResourceId")
+    private BottomNavigationView.OnNavigationItemSelectedListener navItemSelectedListener =
+            item -> {
+                Fragment selectedFragment = null;
+
+                // Determine which fragment to show based on the selected menu item
+                int itemId = item.getItemId();
+                if (itemId == R.id.home_btn_nav) {
+                    selectedFragment = new HomeFragment();
+                } else if (itemId == R.id.list_btn_nav) {
+                    selectedFragment = new ListFragment();
+                } else if (itemId == R.id.add_btn_nav) {
+                    selectedFragment = new AddFragment();
+                } else {
+                    selectedFragment = new ProfileFragment();
+                }
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragmentContainer, selectedFragment)
+                        .commit();
+
+                return true;
+            };
 }
