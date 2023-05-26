4d4635cd87e532d48a4d371ac5e5a33b77fabe78
masa <fmc97masa@gmail.com>
2023-05-26 13:05:46 +0300

    create fragmnet to add note and add task and create three card to show content

--- app/src/main/java/com/masa/minisilm/Ui/AddFragment.java
+++ app/src/main/java/com/masa/minisilm/Ui/AddFragment.java
@@ -2,18 +2,26 @@ package com.masa.minisilm.Ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 import com.masa.minisilm.R;
 
 
 public class AddFragment extends Fragment {
 
-
+    private FragmentAddBinding binding;
+    private Button addTask;
+    private Button addNote;
+    private FrameLayout fragmentContainer;
+    private Fragment currentFragment;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,7 +32,31 @@ public class AddFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false);
+        binding = FragmentAddBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        currentFragment = new Add_TaskFragment();
+        getChildFragmentManager().beginTransaction()
+                .replace(R.id.fragmentadd, currentFragment)
+                .commit();
+
+        binding.addTask.setOnClickListener(v -> {
+            currentFragment = new Add_TaskFragment();
+            getChildFragmentManager().beginTransaction()
+                    .replace(R.id.fragmentadd, currentFragment)
+                    .commit();
+        });
+        binding.addNote.setOnClickListener(v -> {
+            // Switch to Fragment2
+            currentFragment = new Add_NoteFragment();
+            getChildFragmentManager().beginTransaction()
+                    .replace(R.id.fragmentadd, currentFragment)
+                    .commit();
+        });
     }
 }
